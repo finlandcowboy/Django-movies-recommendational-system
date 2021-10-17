1c22e29154bb3c7e9073d9d161843badaf34de14
Stepan <finlandcowboy@gmail.com>
2021-10-17 20:43:32 +0300

    added stat tracker

--- data_parse.py
+++ data_parse.py
@@ -1,4 +1,4 @@
-from os import error
+from os import error, stat_result
 from bs4 import BeautifulSoup
 import requests
 import proxies
@@ -8,6 +8,15 @@ import proxies
 
 url = 'https://www.kinopoisk.ru/top/navigator/'
 base_url = 'https://www.kinopoisk.ru'
+
+
+stat = {
+    'robot_error' : 0,
+    'proxy_connection_error' : 0,
+    'kinopoisk_unknown_error' : 0,
+    'movie_urls_parsed' : 0,
+    'ratings_parsed' : 0
+}
 
 '''
 
@@ -57,17 +66,19 @@ for page in tqdm(kinopoisk_pages):
         if resp.status_code != 200:
             #print(f'Error GET Code {resp.status_code}')
             time.sleep(0.1)
+            stat['proxy_connection_error'] += 1
             continue
         soup = BeautifulSoup(resp.content, features='lxml')
         if 'робот' in soup.text:
             #print('Captcha Error')
             time.sleep(0.1)
+            stat['robot_error'] += 1
             continue
         
         else:
             #print('Connection succesfull')
             break
-
+    print(f'Proxy {proxy} got connection')
     movies_div = soup.find('div', id='itemList')
     if movies_div is None:
         error_dict = {
@@ -75,12 +86,20 @@ for page in tqdm(kinopoisk_pages):
             'url' : page
         }
         error_catcher.append(error_dict)
+        stat['kinopoisk_unknown_error'] += 1
         continue
     for movie_page in movies_div.find_all('div', attrs={'class': 'item _NO_HIGHLIGHT_'}):
         #movie_urls.append(base_url + movie_page.find_all('a')[0].get('href'))
         #kinopoisk_rating.append(movie_page.find('div', attrs={'numVote ratingGreenBG'}).find('span').text.split()[0])
         movies_url_file.write(base_url + movie_page.find_all('a')[0].get('href'))
+        if movie_page.find_all('a')[0].get('href'):
+            stat['movie_urls_parsed'] += 1
         kinopoisk_rating_file.write(movie_page.find('div', attrs={'numVote ratingGreenBG'}).find('span').text.split()[0])
+        if movie_page.find('div', attrs={'numVote ratingGreenBG'}).find('span').text.split()[0]:
+            stat['ratings_parsed'] += 1
     print('Page succesfully parsed')
 
 print('Parsing succesfully complete!')
+
+for metric in stat:
+    print(f'{metric}: {stat[metric]}')
