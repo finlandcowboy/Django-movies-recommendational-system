9916137ad08a5c73be70ff6e327df0da031f6233
Stepan <finlandcowboy@gmail.com>
2021-10-20 13:54:28 +0300

    tqdm

--- top250.py
+++ top250.py
@@ -6,9 +6,10 @@ import random
 from tqdm import tqmd
 movie_end_url = []
 error_count = 0
+proxy_list = proxies.proxy_list
 for page in tqdm(range(5), desc='Page'):
     time.sleep(random.randint(0,10) * error_count)
-    for proxy in tqdm(proxies.proxy_list, desc='Proxy'):
+    for proxy in tqdm(proxy_list, desc='Proxy'):
         url = f'https://www.kinopoisk.ru/lists/top250/?page={page+1}&tab=all'
         print(url)
         resp = requests.get(url, proxies={'https:':proxies.get_proxy(proxy)})
@@ -17,11 +18,13 @@ for page in tqdm(range(5), desc='Page'):
         if 'отправляли' in soup.text:
             error_count += 1
             time.sleep(1 * (1.05**error_count))
+            proxy_list.remove(proxy)
             continue
         
         if not soup.find('div', attrs={'class': 'selection-list'}):
             error_count += 1
             time.sleep(1 * (1.05**error_count))
+            proxy_list.remove(proxy)
             continue
 
         print(soup.text)
